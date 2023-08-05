69988712c0b0c866a46c5de800eb8a59b20b5b8a
ndj <teq.ndj@gmail.com>
2023-08-05 18:54:19 +0900

    Update テキストファイル読み込み.py

--- recipe/テキストファイル読み込み.py
+++ recipe/テキストファイル読み込み.py
@@ -1,5 +1,6 @@
 import sys
 
+#読み込んだファイルを一行ずつのリストに変換
 def open_text(file_name):
     lines = []
     try:
@@ -13,5 +14,21 @@ def open_text(file_name):
 
     return lines
 
+#改行コードを削除する
+def open_text_split_lines(file_name):
+    lines = []
+    try:
+        with open(file_name, mode="r") as f:
+            tmp_lines = f.splitlines()
+            lines = list(tmp_lines)
+        
+    except Exception as e:
+        exc_type, _, exc_tb = sys.exc_info()
+        print("(E open_text error) %s %s: %s" % (exc_tb.tb_lineno, exc_type, e))
+
+    return lines
+
+
 if __name__ == "__main__":
     open_text("test.text")
+    open_text_split_lines(file_name)
